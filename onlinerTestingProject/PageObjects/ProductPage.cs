804235b13d78300d0f8fcff0f022d95041288986
Alexandra Rybakova <sasha600591@gmail.com>
2020-12-07 01:08:23 +0300

    add OpenCartPage to ProductPage

--- onlinerTestingProject/PageObjects/ProductPage.cs
+++ onlinerTestingProject/PageObjects/ProductPage.cs
@@ -7,6 +7,7 @@ namespace OnlinerTestingProject.PageObjects
     public class ProductPage
     {
         private By AddToCartButton = By.XPath("//div[@class='product-aside__box']/a[contains(@class,'item-button')]");
+        private By Cart = By.XPath("//*[@id='userbar']//*[@title='Корзина']");
 
         public void ClickAddToCartButton()
         {
@@ -15,5 +16,15 @@ namespace OnlinerTestingProject.PageObjects
 
             WebDriverFactory.Driver.FindElement(AddToCartButton).Click();
         }
+
+        public void OpenCartPage()
+        {
+            WebDriverWait wait = new WebDriverWait(WebDriverFactory.Driver, TimeSpan.FromSeconds(40));
+            wait.Until(ExpectedConditions.ElementToBeClickable(AddToCartButton));
+
+            WebDriverFactory.Driver.FindElement(Cart).Click();
+        }
+
+
     }
 }
--- onlinerTestingProject/TestCases/BuyItem.cs
+++ onlinerTestingProject/TestCases/BuyItem.cs
@@ -11,7 +11,7 @@ namespace OnlinerTestingProject.TestCases
             Pages.HomePage.SearchItem("Ноутбук Apple MacBook Air 13 2020 MWTJ2");
             Pages.SearchResultPage.ClickItem();
             Pages.ProductPage.ClickAddToCartButton();
-            Pages.ProductPage.ClickAddToCartButton();
+            Pages.ProductPage.OpenCartPage();
 
             Assert.AreEqual("Оформить заказ", Pages.CartPage.GetCheckoutButtonText());
         }
